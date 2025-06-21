a3e3f1b46095f7a164441a0caf7c422c4e4c8fd0
lexuantrung-Peter <le.xuan.trung@moneyforward.vn>
2025-06-21 14:22:44 +0700

    Update site configuration in astro.config.mjs and config.json for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,9 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
-  base: config.site.base_path ? config.site.base_path : "/",
-  trailingSlash: config.site.trailing_slash ? "always" : "never",
+  site: 'https://trungle1612.github.io',
+  base: '/pinwheel-astro',
+  trailingSlash: 'always',
   vite: { 
     plugins: [tailwindcss()],
     optimizeDeps: {
